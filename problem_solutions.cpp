3cbdde2a53f03dd118a73ef124e665b9c9c43171
vaidyakhil <vaidyakhil@gmail.com>
2022-01-25 16:35:17 +0530

    dp and backtracking utils

--- problem_solutions.cpp
+++ problem_solutions.cpp
@@ -4,38 +4,46 @@ class Solutions {
 		** https://www.interviewbit.com/blog/minimum-number-of-jumps/
 		*/
 		static int minJump(vector<int> &A) {
-		    int n = A.size();
-		    int res = 0;
-		    if (n < 2) {
-		        return res;
-		    }
+	        int n = A.size();
+	        int res = 0;
+	        if (n < 2) {
+	            // return true;
+	            return 0;
+	        }
 
-		    int pos = 0;
-		    res++;
-		    while (pos + A[pos] < n-1) {
-		        int i = pos + 1;
-		        int curRadius = pos + A[pos];
-		        while (i <= curRadius) {
-		            if (i + A[i] > pos + A[pos]) {
-		                pos = i;
-		            }
-		            i++;
-		        }
-		        if(pos + A[pos] <= curRadius) {
-		            return -1;
-		        }
-		        res++;
-		    }
-		    return res;
+	        int pos = 0;
+	        int start = pos + 1;
+	        while (pos + A[pos] < n-1) {
+	            int newPos = pos;
+	            int i = start;
+	            int curMax = pos + A[pos];
+	            while (i<=curMax) {
+	                if (i + A[i] > newPos + A[newPos]) {
+	                    newPos = i;
+	                }
+	                i++;
+	            }
+	            if (newPos == pos) {
+	                // return false;
+	                return -1;
+	            }
+	            start = curMax+1;
+	            pos = newPos;
+	            res++;
+	        }
+	        res++;
+	        // return true;
+	        return res;
 		}
 
 		/*
 		*  GOOD PROBLEM USE OF LINKED LIST CYCLE DETECTION ALGO.
+		*  https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
 		*/
-		https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+		
 
 		/*
 		*  GOOD PROBLEM VARIANT OF BUY SELL WITH COOLDOWN PERIOD
+		*  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 		*/
-		https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 }
