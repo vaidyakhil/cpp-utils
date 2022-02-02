814fc24878c5e7771abb60c5cb0cacc93251d645
vaidyakhil <vaidyakhil@gmail.com>
2022-02-02 12:48:05 +0530

    new additions

--- algos_v2.cpp
+++ algos_v2.cpp
@@ -1,3 +1,57 @@
 class AlgosV2 {
+private:
+	int _dsu_getRoot(int a, vector<int>& par, vector<int>& count) {
+		while (par[a] != a) {
+			count[par[a]]-= count[a];
+			par[a] = par[par[a]];
+			count[par[a]]+= count[a];
+			a = par[a];
+		}
+		return a;
+	}
+
 	public:
+		/*
+		**	DISJOINT SET UNION
+		**  _dsu_getRoot-> returns the root of the connected component, root is one which has par[a] as a;
+		** 	root-> while finding root of a node we update its parent as the parent of its parent, 
+		**	so that search for root next time is faster.
+		**	dsu_find-> returns true if two nodes have same roots
+		** 	Union -> if belong to diff groups we set parent of root of a to root of b, if count[b] > count[a];
+		** 	count ensures that we add root of smaller group to root of bigger group and the par[a]= par[par[a]] helps to ensure least skewed data structure of parent array.
+		*/
+
+		bool dsu_find(int a, int b, vector<int>& par, vector<int>& count){
+		    return _dsu_getRoot(a, par, count) == _dsu_getRoot(b, par, count);
+		}
+
+		void dsu_union(int a, int b, vector<int>& par, vector<int>& count){
+		    int root_a=root(a, par), root_b= root(b, par);
+		    
+		    if(root_a != root_b) {
+		        if(count[root_a] >= count[root_b]){
+		            par[root_b]= root_a;
+		            count[root_a]+= count[root_b];
+		        }
+		        else{
+		            par[root_a]= root_b;
+		            count[root_b]+= count[root_a];
+		        }
+		    }
+		    return;
+		}
+
+
+		/*
+		**	Monotonous Stack problems:
+		**	IMP ones being: rain water trapping and area in histogram
+		**	In mono stack problems what we do is we push an element in stack if some condition is met,
+		**	else we start poping elements untill condition is met,
+		**	meanwhile calculate the possible res using the elements that are getting popped
+		** 	since these elements we won't encounter again.
+		** 	In this way mono stack approach sometimes results in O(n) time complexity
+		**	for eg. see problem_solutions::trap for rain water problem
+		*/
+
+
 } 
--- problem_solutions.cpp
+++ problem_solutions.cpp
@@ -37,13 +37,169 @@ class Solutions {
 		}
 
 		/*
-		*  GOOD PROBLEM USE OF LINKED LIST CYCLE DETECTION ALGO.
-		*  https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+		**  GOOD PROBLEM USE OF LINKED LIST CYCLE DETECTION ALGO.
+		**  https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
 		*/
 		
 
 		/*
-		*  GOOD PROBLEM VARIANT OF BUY SELL WITH COOLDOWN PERIOD
-		*  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+		**  GOOD PROBLEM VARIANT OF BUY SELL WITH COOLDOWN PERIOD
+		**  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 		*/
+
+        /*
+        ** EDIT DISTANCE (convert word1 -> word2) in minm operations
+        ** https://leetcode.com/problems/edit-distance/submissions/
+        ** m = word1 size
+        ** n = word2 size
+        */
+
+	    int recurEditDistance(string& word1, string& word2, int i, int j, Vec& dp) {
+	        // base cases
+	        if(dp[j][i] != -1) {
+	            return dp[j][i];
+	        }
+	        int res;
+	        if (word1[i] == word2[j]) {
+	            res = min(
+	                recurEditDistance(word1, word2, i-1, j-1, dp),
+	                1+min(
+	                    recurEditDistance(word1, word2, i, j-1, dp),
+	                    recurEditDistance(word1, word2, i-1, j, dp)
+	                )
+	            );
+	        } else {
+	            res = 1+min(
+	                recurEditDistance(word1, word2, i-1, j-1, dp),
+	                min(
+	                    recurEditDistance(word1, word2, i, j-1, dp),
+	                    recurEditDistance(word1, word2, i-1, j, dp)
+	                )
+	            );
+	        }
+	        return dp[j][i] = res;
+	    }
+	    
+	    int iterativeEditDistance(string& word1, string& word2, Vec& dp) {
+	        int n = word1.size();
+	        int m = word2.size();
+	        
+	        for (int i=1; i<m; i++) {
+	            for (int j=1; j<n; j++) {
+	                
+	                int replaced = dp[i-1][j-1];
+	                int deleted = dp[i-1][j];
+	                int inserted = dp[i][j-1];
+	                
+	                if (word1[j] == word2[i]) {
+	                    dp[i][j] = min(
+	                        replaced, // if char matches we will start comparing next of both, no operation
+	                        1+min(
+	                            deleted,
+	                            inserted
+	                        )
+	                    );
+	                } else {
+	                    dp[i][j] = 1+min(
+	                        replaced,
+	                        min(
+	                            deleted,
+	                            inserted
+	                        )
+	                    );
+	                }
+	            }
+	        }
+	        
+	        return dp[m-1][n-1];
+	    }
+
+
+		/*
+		** 	PASS VAUE BY REFERENCE AS MUCH AS POSSIBLE, CAN SAVE A LOT IN REQUIRED MEMORY!!
+		**	see notes/pass_by_ref_on_memory_usage
+		** 	https://leetcode.com/problems/n-queens/submissions/
+		*/
+
+		
+		/*
+		** 	GOOD PROBLEM, Graph, remove leaf nodes layer by layer;
+		** 	https://leetcode.com/problems/minimum-height-trees/
+		*/
+
+
+		/*
+		** 	GOOD PROBLEM, RAIN WATER TRAPPING PROBLEM;
+		**	multiple approaches: left, right array, mono stack approach and 2 pointer approach
+		**	The two pointer approach being least intuitive but best performant
+		**	stack approach increments in horizontal boxes, while others do in vertical.
+		** 	https://leetcode.com/problems/trapping-rain-water/
+		*/
+	    int trap(vector<int>& arr) {
+	        int n = arr.size();
+	        int res = 0;
+	        int lo=0, hi = n-1;
+	        int lMax = 0, rMax = 0;
+	        while (lo < hi) { 
+	        // we are calculating for all the walls
+
+	        	// 	we get to update the water on lo(th) index 
+	        	//	only when bounding wall (min among l,r max) is lMax;
+        		//	simillarly we get to .. hi(th)..
+        		//	... is rMax;
+	            if (arr[lo] < arr[hi]) {
+	                (arr[lo] > lMax) ?
+	                	(lMax = arr[lo]) : res+= lMax-arr[lo]; 
+	                lo++;
+	            } else {
+	                (arr[hi] > rMax) ?
+                		(rMax = arr[hi]) : res+= rMax-arr[hi];
+	                hi--;
+	            }
+	        }
+	        
+	        return res;
+	    }
+
+
+		/*
+		** 	GOOD PROBLEM, LARGEST RECTANGLE AREA PROBLEM;
+		**	multiple approaches again: left, right array, mono stack approach
+		**	stack approach increments in horizontal boxes, while others do in vertical.
+		**	Both are O(n), O(n), just that stack does it in 1 pass
+		** 	https://leetcode.com/problems/largest-rectangle-in-histogram/
+		*/
+	    int largestRectangleArea(vector<int>& arr) {
+	        int n = arr.size();
+	        int res = 0;
+	        stack<int> st;
+	        int i = 0;
+
+	        // run till the stack is empty
+	        while (i<n || !st.empty()) {
+
+	        	// even when we have traversed whole array, we might have missed comp areas for some
+	        	// think sorted inc array
+	        	// hence the i==n block
+	        	//	we use i =n element as height = 0 element (shorter from all)
+	            while (!st.empty() && ( i == n || arr[st.top()] > arr[i] )) {
+	            	// you got an element which is smaller than the top of stack element
+	            	// this indicates that if we create rect of height of h[st.top]
+	            	// i will be the right boundry for such rect
+	            	// left boundry will be st.top after popping cur.
+	                int cur = st.top();
+	                st.pop();
+
+	                int left = st.empty() ? -1 : st.top();
+	                int right = i;
+	                res = max((right-left-1)*arr[cur], res);
+	            }
+	            if (i<n) {
+	                st.push(i);    
+	                i++;
+	            }
+	        }
+
+	        return res;
+	    }
 }
